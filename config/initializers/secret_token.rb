a1b902fd52c6878923eb6a9186161a5432d094fc
Josh Gwyther <joshua.gwyther@gmail.com>
2014-05-13 14:32:10 -0700

    app with status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vcloudbook::Application.config.secret_key_base = 'ce6b99290c5d5e93d224bef81b9700672252f8fef4b560c139ee1849e09024dc2fca6bcd3e27d5964d4033849bd9d1b888c97704ed24df87937ae8d17a525046'
